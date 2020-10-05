b46b2de22c71513fc2e42c6b04d8cd04795e1b4d
mkmondal <mkmondal@MKMONDAL-LAP.oradev.oraclecorp.com>
2020-10-05 23:16:46 +0530

    commit

--- StarterPreperation/src/main/java/com/prep/tree/AVLTree.java
+++ StarterPreperation/src/main/java/com/prep/tree/AVLTree.java
@@ -1,6 +1,5 @@
 package com.prep.tree;
 
-//Java program for insertion in AVL Tree 
 class NodeAVL {
 	int key, height;
 	NodeAVL left, right;
@@ -14,8 +13,6 @@ class NodeAVL {
 class AVLTree {
 
 	NodeAVL root;
-
-	// A utility function to get the height of the tree
 	int height(NodeAVL N) {
 		if (N == null)
 			return 0;
@@ -23,48 +20,36 @@ class AVLTree {
 		return N.height;
 	}
 
-	// A utility function to get maximum of two integers
 	int max(int a, int b) {
 		return (a > b) ? a : b;
 	}
 
-	// A utility function to right rotate subtree rooted with y
-	// See the diagram given above.
 	NodeAVL rightRotate(NodeAVL y) {
 		NodeAVL x = y.left;
 		NodeAVL T2 = x.right;
 
-		// Perform rotation
 		x.right = y;
 		y.left = T2;
 
-		// Update heights
 		y.height = max(height(y.left), height(y.right)) + 1;
 		x.height = max(height(x.left), height(x.right)) + 1;
 
-		// Return new root
 		return x;
 	}
 
-	// A utility function to left rotate subtree rooted with x
-	// See the diagram given above.
 	NodeAVL leftRotate(NodeAVL x) {
 		NodeAVL y = x.right;
 		NodeAVL T2 = y.left;
 
-		// Perform rotation
 		y.left = x;
 		x.right = T2;
 
-		// Update heights
 		x.height = max(height(x.left), height(x.right)) + 1;
 		y.height = max(height(y.left), height(y.right)) + 1;
 
-		// Return new root
 		return y;
 	}
 
-	// Get Balance factor of node N
 	int getBalance(NodeAVL N) {
 		if (N == null)
 			return 0;
@@ -73,8 +58,6 @@ class AVLTree {
 	}
 
 	NodeAVL insert(NodeAVL node, int key) {
-
-		/* 1. Perform the normal BST insertion */
 		if (node == null)
 			return (new NodeAVL(key));
 
@@ -82,46 +65,32 @@ class AVLTree {
 			node.left = insert(node.left, key);
 		else if (key > node.key)
 			node.right = insert(node.right, key);
-		else // Duplicate keys not allowed
+		else 
 			return node;
 
-		/* 2. Update height of this ancestor node */
 		node.height = 1 + max(height(node.left), height(node.right));
 
-		/*
-		 * 3. Get the balance factor of this ancestor node to check whether this node
-		 * became unbalanced
-		 */
 		int balance = getBalance(node);
 
-		// If this node becomes unbalanced, then there
-		// are 4 cases Left Left Case
 		if (balance > 1 && key < node.left.key)
 			return rightRotate(node);
 
-		// Right Right Case
 		if (balance < -1 && key > node.right.key)
 			return leftRotate(node);
 
-		// Left Right Case
 		if (balance > 1 && key > node.left.key) {
 			node.left = leftRotate(node.left);
 			return rightRotate(node);
 		}
 
-		// Right Left Case
 		if (balance < -1 && key < node.right.key) {
 			node.right = rightRotate(node.right);
 			return leftRotate(node);
 		}
 
-		/* return the (unchanged) node pointer */
 		return node;
 	}
 
-	// A utility function to print preorder traversal
-	// of the tree.
-	// The function also prints height of every node
 	void preOrder(NodeAVL node) {
 		if (node != null) {
 			System.out.print(node.key + " ");
