bec29bf05019061eb2654dda25013531d4dc4fd9
mrinal10 <mrinal.mondal6@gmail.com>
2020-10-11 11:02:12 +0530

    commit msg

--- StarterPreperation/src/main/java/com/hackerrank/problemsolving/trie/Contacts.java
+++ StarterPreperation/src/main/java/com/hackerrank/problemsolving/trie/Contacts.java
@@ -80,33 +80,7 @@ public class Contacts {
 	private static final Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) throws IOException {
-		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-		int queriesRows = Integer.parseInt(scanner.nextLine().trim());
-
-		String[][] queries = new String[queriesRows][2];
-
-		for (int queriesRowItr = 0; queriesRowItr < queriesRows; queriesRowItr++) {
-			String[] queriesRowItems = scanner.nextLine().split(" ");
-
-			for (int queriesColumnItr = 0; queriesColumnItr < 2; queriesColumnItr++) {
-				String queriesItem = queriesRowItems[queriesColumnItr];
-				queries[queriesRowItr][queriesColumnItr] = queriesItem;
-			}
-		}
-
-		int[] result = contacts(queries);
-
-		for (int resultItr = 0; resultItr < result.length; resultItr++) {
-			bufferedWriter.write(String.valueOf(result[resultItr]));
-
-			if (resultItr != result.length - 1) {
-				bufferedWriter.write("\n");
-			}
-		}
-
-		bufferedWriter.newLine();
-
-		bufferedWriter.close();
+		String[][] arr = {{"add", "hacker"},{"add","hack"},{"find", "hac"},{"find", "hak"}};
+		contacts(arr);
 	}
 }
